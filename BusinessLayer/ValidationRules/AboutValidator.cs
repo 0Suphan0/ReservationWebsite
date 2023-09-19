edf33cc8143e83b77e14a83ca456a4b6619a62d5
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-19 17:09:59 +0300

    Projeye FluentValidation entegre edildi. Tema dosyası projeye aktarıldı. About entity için validasyon kuralları yazıldı.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator: AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama alanı boş bırakılamaz.");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen resim giriniz.");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Minimum 50 karakter girilmelidir.");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Maximum 1500 karakter girilmelidir.");
+
+
+
+        }
+    }
+}
--- ReservationApp/Startup.cs
+++ ReservationApp/Startup.cs
@@ -50,7 +50,7 @@ namespace ReservationApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
