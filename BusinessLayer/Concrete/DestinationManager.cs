8802c2a1ad22439b55c04efa1001a11bae993866
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-21 01:21:27 +0300

    Tur rotalarının veritabanından çekilip listelenmesi işlemi tamamlandı.

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        private IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+
+
+        public List<Destination> GetAll()
+        {
+           return _destinationDal.GetAll();
+        }
+
+        public Destination GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TRemove(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Destination entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReservationApp/ViewComponents/Default/_PopularDestinationPartial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.ViewComponents.Default
+{
+    public class _PopularDestinationPartial:ViewComponent
+    {
+        private DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.GetAll();
+            return View(values);
+        }
+    }
+}
