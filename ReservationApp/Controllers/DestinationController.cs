2f2d28c9db717f76f924a44095313ceb5ebfdba6
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-23 14:03:48 +0300

    Tur rotaları listeleme işlemi tamamlandı.

---
+++ ReservationApp/Controllers/DestinationController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Controllers
+{
+    public class DestinationController : Controller
+    {
+        private DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+
+        public IActionResult Index()
+        {
+            var values=destinationManager.GetAll();
+            return View(values);
+        }
+    }
+}
