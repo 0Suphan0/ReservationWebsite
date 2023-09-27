b0dc982a6e977677327c6d7168102d701e09d015
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-27 22:40:05 +0300

    Login ve Register sayfaları arayüzleri olusturuldu. Login controller eklendi.

---
+++ ReservationApp/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
