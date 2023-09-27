e8161c36a77c15aebf59777e4a3618291fb04813
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-27 21:56:40 +0300

    Login,Registeri,Rolleme gibi islemler icin identity kütüphanesi eklendi. AppRole ve AppUser sınıfları olusturuldu.

---
+++ EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace EntityLayer.Concrete
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string ImageUrl { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
