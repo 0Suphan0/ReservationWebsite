a51fba8b807a6412c1c21088cbd5eecedff504a4
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-29 19:48:20 +0300

    Password girişi için CustomIdentityValidator yazıldı böylece hata mesajları istenilen şekilde gösterildi.

--- ReservationApp/Startup.cs
+++ ReservationApp/Startup.cs
@@ -13,61 +13,64 @@ using DataAccessLayer.Concrete;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using ReservationApp.Models;
 
 namespace ReservationApp
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-	        services.AddDbContext<Context>();
-	        services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
-	        services.AddControllersWithViews();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<Context>();
+			services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>()
+				.AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
 
-	        services.AddMvc(config =>
-	        {
-		        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser()
-			        .Build();
-		        config.Filters.Add(new AuthorizeFilter(policy));
-	        });
+			services.AddControllersWithViews();
 
-	        services.AddMvc();
-        }
+			services.AddMvc(config =>
+			{
+				var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser()
+					.Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+			});
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseAuthentication();
-            app.UseRouting();
+			services.AddMvc();
+		}
 
-            app.UseAuthorization();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseAuthentication();
+			app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Default}/{action=Index}/{id?}");
-            });
-        }
-    }
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Default}/{action=Index}/{id?}");
+			});
+		}
+	}
 }
