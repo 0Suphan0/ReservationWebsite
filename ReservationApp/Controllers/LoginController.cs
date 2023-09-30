185967f23040c8776d9583e312fef2476e1f1584
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-10-01 00:04:02 +0300

    Login islemi tamamlandı.

--- ReservationApp/Controllers/LoginController.cs
+++ ReservationApp/Controllers/LoginController.cs
@@ -13,9 +13,12 @@ namespace ReservationApp.Controllers
 
         private readonly UserManager<AppUser> _userManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
-	        _userManager = userManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
 
@@ -57,6 +60,26 @@ namespace ReservationApp.Controllers
         [HttpGet]
         public IActionResult SignIn()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel userSignInViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(userSignInViewModel.Username,
+                    userSignInViewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn", "Login");
+                }
+            }
+
             return View();
         }
     }
