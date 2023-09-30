9d46ca278a25382a12ab8d57231e0fd2b0a8f6b2
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-09-30 22:06:45 +0300

    Üye sayfasında rotaların tabloda listelenmesi işlemi tamamlandı.

---
+++ ReservationApp/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Areas.Member.Controllers
+{
+    [AllowAnonymous]
+    public class DestinationController : Controller
+    {
+        private DestinationManager _destinationManager = new DestinationManager(new EfDestinationDal());
+
+
+        [Area("Member")]
+
+        public IActionResult Index()
+        {
+            var values = _destinationManager.GetAll();
+            return View(values);
+        }
+    }
+}
