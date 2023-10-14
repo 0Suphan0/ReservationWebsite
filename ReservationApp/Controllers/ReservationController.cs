6f880577ab1b3a7842358a74621c7b799d216a88
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2023-10-14 11:07:35 +0300

    Reservations tablosu eklendi.

---
+++ ReservationApp/Controllers/ReservationController.cs
@@ -0,0 +1,35 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationApp.Controllers
+{
+    [Area("Member")]
+    public class ReservationController : Controller
+    {
+
+        public IActionResult MyCurrentReservation()
+        {
+            return View();
+        }
+
+        public IActionResult MyOldReservation()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult NewReservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewReservation(Reservation reservation)
+        {
+            return View();
+        }
+
+
+    }
+}
